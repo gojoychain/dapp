23fc69099f21d19141cddc813bea6de419199f15
Deric Walintukan <deric.walintukan@gmail.com>
2019-03-25 22:40:27 +0700

    Dont throw for mainnet mining contracts

--- src/contracts/poi.js
+++ src/contracts/poi.js
@@ -1,22 +1,21 @@
 import web3 from '../web3';
 import { CHAIN_ID } from '../config';
 
-// const mainnetAddr = '';
+// TODO: replace mainnet with real address after deployed
+const mainnetAddr = '0xD4dBc10ef8b6BE71E67aac803D0f53F8294346E2';
 const testnetAddr = '0xD4dBc10ef8b6BE71E67aac803D0f53F8294346E2';
 const abi = [{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"interval"}],"name":"withdrawInterval","inputs":[],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[{"type":"bool","name":"success"}],"name":"withdraw","inputs":[],"constant":false},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"amount"}],"name":"withdrawAmount","inputs":[],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"renounceOwnership","inputs":[],"constant":false},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"address","name":""}],"name":"owner","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"bool","name":""}],"name":"isOwner","inputs":[],"constant":true},{"type":"function","stateMutability":"view","payable":false,"outputs":[{"type":"uint256","name":"lastBlock"}],"name":"lastWithdrawBlock","inputs":[],"constant":true},{"type":"function","stateMutability":"nonpayable","payable":false,"outputs":[],"name":"transferOwnership","inputs":[{"type":"address","name":"newOwner"}],"constant":false},{"type":"constructor","stateMutability":"nonpayable","payable":false,"inputs":[{"type":"address","name":"owner"}]},{"type":"fallback","stateMutability":"payable","payable":true},{"type":"event","name":"OwnershipTransferred","inputs":[{"type":"address","name":"previousOwner","indexed":true},{"type":"address","name":"newOwner","indexed":true}],"anonymous":false},{"type":"event","name":"Withdrawal","inputs":[{"type":"address","name":"to","indexed":true},{"type":"uint256","name":"amount","indexed":false}],"anonymous":false}]; // eslint-disable-line
 
-// let mainnet;
+let mainnet;
 let testnet;
 
 export default () => {
   switch (web3.givenProvider.networkVersion) {
     case CHAIN_ID.MAINNET: {
-      throw Error('Mainnet contract not deployed yet.');
-      // TODO: uncomment when mainnet is deployed
-      // if (!mainnet) {
-      //   mainnet = new web3.mainnet.eth.Contract(abi, mainnetAddr);
-      // }
-      // return mainnet;
+      if (!mainnet) {
+        mainnet = new web3.eth.Contract(abi, mainnetAddr);
+      }
+      return mainnet;
     }
     case CHAIN_ID.TESTNET: {
       if (!testnet) {
