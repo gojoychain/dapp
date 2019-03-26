2687b8a06734697274e5eeba60929e0273d3c766
Deric Walintukan <deric.walintukan@gmail.com>
2019-03-26 13:53:10 +0700

    Add create token page

---
+++ src/components/CreateToken/index.js
@@ -0,0 +1,59 @@
+import React, { Component, Fragment } from 'react';
+import { withStyles, Typography } from '@material-ui/core';
+
+import styles from './styles';
+import TabContentContainer from '../TabContentContainer';
+import ContractInfoContainer from '../ContractInfoContainer';
+import web3 from '../../web3';
+
+class CreateToken extends Component {
+  state = {
+  };
+
+  async componentDidMount() {
+    this.initState();
+  }
+
+  componentDidUpdate(prevProps) {
+    // const { mmLoaded } = this.props;
+    // if (prevProps.mmLoaded !== mmLoaded) {
+    //   this.initState();
+    // }
+  }
+
+  // initState = async () => {
+  //   const { currentAddress } = this.props;
+  //   if (!currentAddress || !web3) return;
+
+  //   const owner = await GHUSD().methods.owner().call();
+  //   const ghusdBalance = await GHUSD().methods.balanceOf(currentAddress).call();
+  //   const balance = await web3.eth.getBalance(currentAddress);
+  //   this.setState({ owner, ghusdBalance, balance });
+  // }
+
+  handleChange = name => (event) => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes, currentAddress } = this.props;
+
+    if (!currentAddress || !web3) {
+      return <div />;
+    }
+
+    return (
+      <TabContentContainer>
+        <ContractInfoContainer>
+          <Typography variant="h4" className={classes.heading}>
+            Create GRC223 Token
+          </Typography>
+        </ContractInfoContainer>
+      </TabContentContainer>
+    );
+  }
+}
+
+export default withStyles(styles)(CreateToken);
---
+++ src/components/CreateToken/styles.js
@@ -0,0 +1,5 @@
+export default theme => ({
+  heading: {
+    marginBottom: theme.spacing.threeX,
+  },
+});
