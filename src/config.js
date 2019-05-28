b587105273daafb4f1066fa453c14f6271c68080
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-28 10:56:56 +0700

    Change config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
   PROVIDER: {
-    MAINNET: 'https://api.ghuchain.com',
-    TESTNET: 'https://testapi.ghuchain.com',
+    MAINNET: 'https://api.gojoychain.com',
+    TESTNET: 'https://testapi.gojoychain.com',
   },
   CHAIN_ID: {
     MAINNET: '18',
