95b1aa35ef573e4c9ea225dece25b5608b7e0fb8
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-26 10:42:26 +0700

    Update web3

--- src/contracts/jusd.js
+++ src/contracts/jusd.js
@@ -13,6 +13,7 @@ export default () => {
     case CHAIN_ID.MAINNET: {
       if (!mainnet) {
         mainnet = new web3.eth.Contract(abi, mainnetAddr);
+        console.log(mainnet);
       }
       return mainnet;
     }
